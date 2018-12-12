da3e9763b8f5a9b4758b9e49e8657be333469070
mark.a.r.madrona <mark.a.r.madrona@accenture.com>
2018-12-12 17:08:18 +0800

    angular-directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -22,6 +23,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
